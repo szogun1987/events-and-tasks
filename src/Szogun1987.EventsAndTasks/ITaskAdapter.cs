977da168499eac86eaefcdbeebd10bfbccb0cfe1
Wojciech Gomoła <szogun1987@gmail.com>
2017-02-14 10:27:37 +0100

    Interface declarations:

---
+++ src/Szogun1987.EventsAndTasks/ITaskAdapter.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Szogun1987.EventsAndTasks
+{
+    public interface ITaskAdapter
+    {
+        Task<int> GetNextInt();
+
+        Task<int> GetNextIntWithArg(string arg);
+    }
+}
---
+++ src/Szogun1987.EventsAndTasks/ITrigger.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Szogun1987.EventsAndTasks
+{
+    /// <summary>
+    /// Allows triggering from tests
+    /// </summary>
+    interface ITrigger
+    {
+        void Release();
+    }
+}
