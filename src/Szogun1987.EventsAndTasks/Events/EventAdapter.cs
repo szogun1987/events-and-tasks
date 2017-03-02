181bc667ebedd7fc30ad3bdaf1a7c4ceeeaa15cb
Wojciech Gomoła <szogun1987@gmail.com>
2017-03-02 10:54:16 +0100

    Event to task builder

--- src/Szogun1987.EventsAndTasks/Events/EventAdapter.cs
+++ src/Szogun1987.EventsAndTasks/Events/EventAdapter.cs
@@ -7,18 +7,19 @@ namespace Szogun1987.EventsAndTasks.Events
     {
         private readonly EventApi _eventApi;
 
-        private readonly EventToTask<ResultEventArgs> _getNextIntEventToTask;
+        private readonly EventToTask<EventApi, ResultEventArgs> _getNextIntEventToTask;
 
         public EventAdapter(EventApi eventApi)
         {
             _eventApi = eventApi;
 
-            _getNextIntEventToTask = new EventToTask<ResultEventArgs>(
-                handler => _eventApi.GetNextIntCompleted += handler,
-                handler => _eventApi.GetNextIntCompleted -= handler,
-                handler => _eventApi.GetNextIntFailed += handler,
-                handler => _eventApi.GetNextIntFailed -= handler,
-                () => _eventApi.GetNextInt());
+            _getNextIntEventToTask = new EventToTask<EventApi, ResultEventArgs>(
+                _eventApi,
+                (context, handler) => context.GetNextIntCompleted += handler,
+                (context, handler) => context.GetNextIntCompleted -= handler,
+                (context, handler) => context.GetNextIntFailed += handler,
+                (context, handler) => context.GetNextIntFailed -= handler,
+                context => context.GetNextInt());
         }
         
         public async Task<int> GetNextInt()
@@ -29,12 +30,13 @@ namespace Szogun1987.EventsAndTasks.Events
 
         public async Task<int> GetNextIntWithArg(string arg)
         {
-            var eventToTask = new EventToTask<ResultEventArgs>(
-                handler => _eventApi.GetNextIntWithArgCompleted += handler,
-                handler => _eventApi.GetNextIntWithArgCompleted -= handler,
-                handler => _eventApi.GetNextIntWithArgFailed += handler,
-                handler => _eventApi.GetNextIntWithArgFailed -= handler,
-                () => _eventApi.GetNextIntWithArg(arg));
+            var eventToTask = new EventToTask<EventApi, ResultEventArgs>(
+                _eventApi,
+                (context, handler) => context.GetNextIntWithArgCompleted += handler,
+                (context, handler) => context.GetNextIntWithArgCompleted -= handler,
+                (context, handler) => context.GetNextIntWithArgFailed += handler,
+                (context, handler) => context.GetNextIntWithArgFailed -= handler,
+                context => context.GetNextIntWithArg(arg));
 
             var eventArgs = await eventToTask.Invoke();
             return eventArgs.Result;
--- src/Szogun1987.EventsAndTasks/Events/EventToTask.cs
+++ src/Szogun1987.EventsAndTasks/Events/EventToTask.cs
@@ -54,4 +54,59 @@ namespace Szogun1987.EventsAndTasks.Events
             return completionSource.Task;
         }
     }
+
+    public class EventToTask<TContext, TResult>
+    {
+        private readonly TContext _context;
+        private readonly Action<TContext, EventHandler<TResult>> _subscribe;
+        private readonly Action<TContext, EventHandler<TResult>> _unsubscribe;
+        private readonly Action<TContext, EventHandler<FailureEventArgs>> _subscribeFailure;
+        private readonly Action<TContext, EventHandler<FailureEventArgs>> _unsubscribeFailure;
+        private readonly Action<TContext> _trigger;
+
+        public EventToTask(
+            TContext context,
+            Action<TContext, EventHandler<TResult>> subscribe,
+            Action<TContext, EventHandler<TResult>> unsubscribe,
+            Action<TContext, EventHandler<FailureEventArgs>> subscribeFailure,
+            Action<TContext, EventHandler<FailureEventArgs>> unsubscribeFailure,
+            Action<TContext> trigger)
+        {
+            _context = context;
+            _subscribe = subscribe;
+            _unsubscribe = unsubscribe;
+            _subscribeFailure = subscribeFailure;
+            _unsubscribeFailure = unsubscribeFailure;
+            _trigger = trigger;
+        }
+
+        public Task<TResult> Invoke()
+        {
+            var completionSource = new TaskCompletionSource<TResult>();
+
+            EventHandler<TResult> successHandler = null;
+            EventHandler<FailureEventArgs> failureHandler = null;
+
+            successHandler = (sender, args) =>
+            {
+                _unsubscribe(_context, successHandler);
+                _unsubscribeFailure(_context, failureHandler);
+                completionSource.TrySetResult(args);
+            };
+
+            failureHandler = (sender, args) =>
+            {
+                _unsubscribe(_context, successHandler);
+                _unsubscribeFailure(_context, failureHandler);
+                completionSource.TrySetException(args.Error);
+            };
+
+            _subscribe(_context, successHandler);
+            _subscribeFailure(_context, failureHandler);
+
+            _trigger(_context);
+
+            return completionSource.Task;
+        }
+    }
 }
